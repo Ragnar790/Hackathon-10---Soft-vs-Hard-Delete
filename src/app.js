2fc4eb537b859f1c63c977032f7c3aac3830a608
Ragnar790 <kaucykroy@gmail.com>
2021-01-11 20:42:23 +0530

    first try

--- src/app.js
+++ src/app.js
@@ -1,33 +1,69 @@
-const express = require('express');
-const Student = require('./models/Student');
-
+const express = require("express");
+const Student = require("./models/Student");
 
 const app = express();
 
-// middleware 
+// middleware
 app.use(express.json());
 
 // Routes
 
 // Get all the students
-app.get('/students', async (req, res) => {
-    // write your codes here
-})
+app.get("/students", async (req, res) => {
+	// write your codes here
+	res.send(await Student.find());
+});
 
 // Add student to database
-app.post('/students', async (req, res) =>{
-    // write your codes here
-})
+app.post("/students", async (req, res) => {
+	const newStudent = new Student({
+		name: req.body.name,
+		sex: req.body.sex,
+		class: req.body.class,
+		age: req.body.age,
+		grade_point: req.body.grade,
+	});
+	await newStudent.save();
+	res.send(newStudent);
+});
 
 // Get specific student
-app.get('/students/:id', async (req, res) =>{
-    // write your codes here
-})
+app.get("/students/:id", async (req, res) => {
+	try {
+		const doc = await Student.findOne({ _id: req.params.id });
+		if (doc == null) {
+			res.sendStatus(404);
+		} else {
+			res.send(doc);
+		}
+	} catch (err) {
+		res.sendStatus(404);
+	}
+});
 
 // delete specific student
-app.delete('/students/:id', async (req, res) =>{
-    // write your codes here
-}) 
-
+app.delete("/students/:id", async (req, res) => {
+	if (req.query.type === "hard") {
+		try {
+			await Student.deleteOne({ _id: req.params.id });
+			res.sendStatus(200);
+		} catch (err) {
+			res.sendStatus(404);
+		}
+	} else if (req.query.type === "soft") {
+		try {
+			const doc = await Student.findOne({ _id: req.params.id });
+			if (doc == null) {
+				res.sendStatus(404);
+			} else {
+				doc.isDeleted = true;
+				await doc.save();
+				res.sendStatus(200);
+			}
+		} catch (err) {
+			res.sendStatus(404);
+		}
+	}
+});
 
 module.exports = app;
